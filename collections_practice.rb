3e44869e65b03151ca05d54da86dda6e6a03387d
wachka06 <wachka06@gmail.com>
2018-07-13 15:06:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,6 +54,7 @@ def count_elements(array)
     key_array << hash.keys 
   end
   binding.pry
+  
   array.each do |hash|
     if key_array.include?(hash[:name])
       hash[:count] = key_array.count(hash[:name])
