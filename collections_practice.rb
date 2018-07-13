dcfc1252591432117c5b9eebd9363bb005b143c6
wachka06 <wachka06@gmail.com>
2018-07-13 03:33:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
 # your code goes here
+
+def begins_with_r(array)
+  i = 0 
+  new_array = []
+  
+  while i < array.length 
+    if yield(array[i])
+      new_array << array[i]
+    end
+    i += 1 
+  end
+  new_array
+end
+
+def begins_with_r(array)
+  array.all?{|word| word[0] == "r"}
+end
+
+def contain_a(array) 
+  array.select{|word| word.include?("a") == true}
+end
+
+def first_wa(array)
+  array.find{|word| word.to_s.include?("wa") == true}
+end
+
+def remove_non_strings(array)
+  array.delete_if{|word| word.class != String}
+end
+
+def count_elements(array)
+  # array.keys.count{|word| word.to_s.include?("ashley") || word.to_s.include?("blake")}
+  
+  result = array
+  
+  result.each do |hash| 
+    hash.each do |key, val|
+      if key.to_s.include?("ashley") || key.to_s.include?("blake")
+        
+      end
+    end
+  end
+  
+end
+
